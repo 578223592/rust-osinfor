1145dd2c0f226bee81d4a262355bbd4d5f039277
siwuxie <cwl990110@163.com>
2024-05-04 12:36:19 +0800

    add more tool:ip address && os version

--- README.md
+++ README.md
@@ -11,16 +11,18 @@ Rust开发的系统工具，使用RustRover作为Ide。
 - [ ] 获取系统软件包数量；
 - [ ] 获取系统硬件信息(机器型号，bios信息等);
 - [ ] 磁盘使用情况或者格式化磁盘；
-- [ ] 获取系统ip;
-- [ ] 获取系统版本；
+- [x] 获取系统ip;
+- [x] 获取系统版本；
 - [ ] 修改管理员密码；
 
 
 ## 截图展示
 
-获取系统架构;
+- 获取系统架构
 ![image](https://github.com/578223592/rust-osinfor/assets/65906820/6bbac025-0ba5-4ecf-86dd-8e22974cd861)
 
-展示本工具的使用方法;
+- 展示本工具的使用方法
 ![image](https://github.com/578223592/rust-osinfor/assets/65906820/b7499827-abad-42d5-b943-67818462afea)
 
+- 展示Ubuntu系统输出系统pkg的数量
+![img.png](img.png)
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,79 @@
 use structopt::StructOpt;
+use gethostname::gethostname;
+use local_ip_address::local_ip;
+use sys_info::os_release;
+use std::fs;
+use std::process::Command;
 
 #[derive(StructOpt, Debug)]
 struct Opt {
     /// show architecture
     #[structopt(short = "a", long = "arch")]
     arch: bool,
+
+    /// show hostname
+    #[structopt(short = "h", long = "hostname")]
+    host_name: bool,
+
+    /// show IP address
+    #[structopt(short = "p", long = "ipaddress")]
+    ip_address: bool,
+
+    /// show OS version
+    #[structopt(short = "v", long = "OSVersion")]
+    os_version: bool,
+
+    /// show num of pkg
+    #[structopt(short = "n", long = "numpkg")]
+    num_pkg: bool,
 }
 
+fn show_num_pkgs() {
+    if let Ok(os_release) = fs::read_to_string("/etc/os-release") {
+        if os_release.contains("ubuntu") ||os_release.contains("debian")||os_release.contains("mint"){
+            let output = Command::new("sh")
+                .arg("-c")
+                .arg("dpkg -l | grep ^ii | wc -l")
+                .output()
+                .expect("failed to execute process");
+            if output.status.success() {
+                let result = String::from_utf8_lossy(&output.stdout);
+                print!("{}", result); //no new line
+            } else {
+                let error = String::from_utf8_lossy(&output.stderr);
+                eprintln!("Command failed: {}", error);
+            }
+        } else if os_release.contains("rhel") || os_release.contains("centos")||os_release.contains("fedora") {
+            //TODO R系列系统的输出
+            println!("Red Hat系列");
+        } else {
+            println!("未知 Linux 发行版");
+        }
+    } else {
+        println!("无法读取系统信息文件");
+    }
+}
 
 fn main() {
     let opt = Opt::from_args();
     if opt.arch {
         println!("System arch: {}", std::env::consts::ARCH);
+    } else if opt.host_name {
+        println!("Hostname: {:?}", gethostname());
+    }else if opt.ip_address{
+        match local_ip() {
+            Ok(ip) => println!("IP address: {:?}", ip),
+            Err(e) => println!("get IP address Error: {}", e),
+        }
+    }else if opt.os_version{
+        match os_release() {
+            Ok(release) => println!("OS Version: {}", release),
+            Err(e) => println!("OS version error: {}", e),
+        }
+    }else if opt.num_pkg{
+        show_num_pkgs();
     }
+
     // println!("{:?}", opt);
     // println!("Hello, world!");
 }
